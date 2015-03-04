d00b34a3af9c7fea4dc22bf7b7f76d847a45aa69
Tom Mertz & Daniel Toader <student@epicodus.com>
2015-03-04 12:00:14 -0800

    add results page

---
+++ views/results.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Here's Where You've Been</title>
+    </head>
+    <body>
+        <h3>Here's where you've been:</h3>
+        <p>{{ place.getPlace }}</p>
+    </body>
+</html>
