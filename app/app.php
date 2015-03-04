f47bfc2999e3f8582cb09070fee5faaf6f3c27a6
Tom Mertz & Daniel Toader <student@epicodus.com>
2015-03-04 11:59:58 -0800

    add routes to places

--- app/app.php
+++ app/app.php
@@ -1,14 +1,20 @@
 <?php
 
     require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/place.php';
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    $app->get("/", function() use($app) {        
+    $app->get("/", function() use($app) {
         return $app['twig']->render('places.php');
     });
 
+    $app->post("/results", function() use($app) {
+        $place = new Place($_POST['placeName']);
+
+        return $app['twig']->render('results.php', array('place' => $place));
+    });
 
     return $app;
 
